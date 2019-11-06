aacae81741382a1434733cb7601f5ca46c2c896e
ali-shahKust <syed.ali.shah3938@gmail.com>
2019-11-06 09:39:05 +0500

    6  Nov Project

---
+++ CheckItemListBox/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace CheckItemListBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.myList = new System.Windows.Forms.CheckedListBox();
+            this.addItemBtn = new System.Windows.Forms.Button();
+            this.ProsPecBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // myList
+            // 
+            this.myList.FormattingEnabled = true;
+            this.myList.Location = new System.Drawing.Point(254, 83);
+            this.myList.Name = "myList";
+            this.myList.Size = new System.Drawing.Size(171, 214);
+            this.myList.TabIndex = 0;
+            // 
+            // addItemBtn
+            // 
+            this.addItemBtn.Location = new System.Drawing.Point(266, 304);
+            this.addItemBtn.Name = "addItemBtn";
+            this.addItemBtn.Size = new System.Drawing.Size(149, 23);
+            this.addItemBtn.TabIndex = 1;
+            this.addItemBtn.Text = "Add Data";
+            this.addItemBtn.UseVisualStyleBackColor = true;
+            this.addItemBtn.Click += new System.EventHandler(this.addItemBtn_Click);
+            // 
+            // ProsPecBtn
+            // 
+            this.ProsPecBtn.Location = new System.Drawing.Point(432, 83);
+            this.ProsPecBtn.Name = "ProsPecBtn";
+            this.ProsPecBtn.Size = new System.Drawing.Size(75, 23);
+            this.ProsPecBtn.TabIndex = 2;
+            this.ProsPecBtn.Text = "Price ";
+            this.ProsPecBtn.UseVisualStyleBackColor = true;
+            this.ProsPecBtn.Click += new System.EventHandler(this.ProsPecBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ProsPecBtn);
+            this.Controls.Add(this.addItemBtn);
+            this.Controls.Add(this.myList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox myList;
+        private System.Windows.Forms.Button addItemBtn;
+        private System.Windows.Forms.Button ProsPecBtn;
+    }
+}
+
---
+++ CheckItemListBox/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CheckItemListBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void addItemBtn_Click(object sender, EventArgs e)
+        {
+            myList.Items.Add("BS-CS" , CheckState.Checked);
+            myList.Items.Add("BS-SE", CheckState.Indeterminate);
+            myList.Items.Add("MCS", CheckState.Unchecked);
+            myList.Items.Add("MS", CheckState.Unchecked);
+            myList.Items.Add("PHD", CheckState.Unchecked);
+        }
+
+        private void ProsPecBtn_Click(object sender, EventArgs e)
+        {
+            int price=0;
+            int i , p1 = 0, p2 = 0 , p3 = 0, p4 = 0 , p5 = 0;
+            String s = "Checked Programs \n";
+            for (i = 0; i <= (myList.Items.Count - 1); i++)
+            {
+
+                if (myList.GetItemChecked(i))
+                {
+                    if (i == 0)
+                    {
+                        p1 = 10;
+                        price = price + p1 + p2 + p3 + p4;
+                    } 
+                    if(i== 1)
+                    {
+                        p2 = 20;
+                        price = price + p1 + p2 + p3 + p4;
+                    }
+
+                    if (i == 2)
+                    {
+                        p2 = 30;
+                        price = price + p1 + p2+p3+p4;
+                    }
+                    if (i == 3)
+                    {
+                        p2 = 50;
+                        price = price + p1 + p2 + p3 + p4;
+                    }
+                    if (i == 4)
+                    {
+                        p2 = 100;
+                        price = price + p1 + p2 + p3 + p4;
+                    }
+
+
+
+                }
+            }
+            MessageBox.Show("Total Price For Prospectus is " + price);
+        }
+    }
+}
